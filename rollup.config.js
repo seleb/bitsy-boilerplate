d056f60a7547f4df3cec226b1a4ce7a6df47d2e9
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-08-22 00:40:49 -0400

    add some comments explaining `external` use

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,14 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import base from "@bitsy/hecks/rollup.config.base";
 
-const external = {};
+const external = {
+	// entries here can be used to separate dependencies from the generated bundle
+	// in some cases (e.g. babylonjs for the 3D hack, shown below)
+	// this will result in a drastically faster build,
+	// but remember that the dependency will need to be included manually after the fact
+
+	// 'babylonjs': 'window.BABYLON',
+};
 
 export default {
 	...base,
